c1dd9ca0344c77fbf089c3a937ae5a5df31a9c4f
Philip Fletcher <philip.fletcher@stutchbury.com>
2022-02-08 15:54:43 +0000

    Initial commit of src files

---
+++ src/EventJoystick.cpp
@@ -0,0 +1,89 @@
+/**
+ *
+ * GPLv2 Licence https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * 
+ * Copyright (c) 2022 Philip Fletcher <philip.fletcher@stutchbury.com>
+ * 
+ */
+
+
+#include "EventJoystick.h"
+
+
+EventJoystick::EventJoystick(byte analogX, byte analogY)
+  : x(analogX), y(analogY) {
+  x.setStartValue(analogRead(analogX));
+  y.setStartValue(analogRead(analogY));
+  setCentreBoundary(200);
+}
+EventJoystick::EventJoystick() {
+  enable(false,false);
+}
+
+
+void EventJoystick::update() {
+  x.update();
+  y.update();
+  //Serial.printf("x: %i, %i, y: %i, %i \n", x.position(), x.previousPosition(), y.position(), y.previousPosition());
+  if ( x.hasChanged() || y.hasChanged() ) {
+    if (changed_cb != NULL) changed_cb(*this);
+  }
+}
+
+void EventJoystick::setChangedHandler(EventJoystickCallback f) { changed_cb = f; }
+
+void EventJoystick::setIdleHandler(EventJoystickCallback f) { idle_cb = f; }
+
+void EventJoystick::setNumIncrements(uint8_t numIncr /*=10*/) {
+  x.setNumIncrements(numIncr);
+  y.setNumIncrements(numIncr);
+}
+
+void EventJoystick::setNumNegativeIncrements(uint8_t numIncr /*=10*/) {
+  x.setNumNegativeIncrements(numIncr);
+  y.setNumNegativeIncrements(numIncr);
+}
+
+void EventJoystick::setNumPositiveIncrements(uint8_t numIncr /*=10*/) {
+  x.setNumPositiveIncrements(numIncr);
+  y.setNumPositiveIncrements(numIncr);
+}
+
+void EventJoystick::setCentreBoundary(uint16_t width /*=200*/) {
+  x.setStartBoundary(width);
+  y.setStartBoundary(width);
+}
+
+void EventJoystick::setOuterBoundary(uint16_t width /*=100*/) {
+  x.setEndBoundary(width);
+  y.setEndBoundary(width);
+}
+
+void EventJoystick::setIdleTimeout(unsigned int timeoutMs) {
+  x.setIdleTimeout(timeoutMs);
+  y.setIdleTimeout(timeoutMs);
+}
+
+
+bool EventJoystick::hasChanged() {
+  return x.hasChanged() || y.hasChanged();
+}
+
+unsigned int EventJoystick::userId() { return _userId; }
+
+unsigned int EventJoystick::userState() { return _userState; }
+
+bool EventJoystick::enabled() { return x.enabled() && y.enabled(); }
+
+void EventJoystick::enable(bool e, bool allowRead) { 
+  x.enable(e, allowRead);
+  y.enable(e, allowRead);
+}
+
+
+bool EventJoystick::isIdle() {
+  //Return true if idle (whether idle callback defined or not)
+  return x.isIdle() && y.isIdle();
+}
+
+
---
+++ src/EventJoystick.h
@@ -0,0 +1,180 @@
+/**
+ * Based on the EventAnalog library, this is an event based library for 
+ * analog joysticks - slice an analog range of each axis into configurable 
+ * number of increments and a callback will be fired on each change of increment.
+ * Effective noise reduction built in.
+ * Also has an idle timeout callback.
+ * 
+ * Use EventButton to register event for the joystick button.
+ * 
+ * GPLv2 Licence https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * 
+ * Copyright (c) 2022 Philip Fletcher <philip.fletcher@stutchbury.com>
+ * 
+ */
+
+#include "Arduino.h"
+
+#include <EventAnalog.h>
+
+#ifndef EventJoystick_h
+#define EventJoystick_h
+
+#if defined(ARDUINO_ARCH_ESP32) || defined(ESP8266) || defined(CORE_TEENSY)
+  #include <functional>
+#endif
+
+class EventJoystick {
+
+  protected:
+
+    #if defined(ARDUINO_ARCH_ESP32) || defined(ESP8266) || defined(CORE_TEENSY)
+      typedef std::function<void(EventJoystick &btn)> EventJoystickCallback;
+    #else
+      typedef void (*EventJoystickCallback)(EventJoystick &);
+    #endif
+
+  public:
+
+    EventAnalog x;
+    EventAnalog y;
+
+    /**
+     * Construct a joystick
+     */
+    EventJoystick(byte analogX, byte analogY);
+
+    EventJoystick();
+
+    void update();
+
+
+    /** ***************************************************
+     * set joystick callback handlers
+     */
+
+    /**
+     */
+    void setChangedHandler(EventJoystickCallback f);
+    /**
+     */
+    void setIdleHandler(EventJoystickCallback f);
+    
+    /** ***************************************
+     *  joystick setup
+     */
+
+    /**
+     * @brief Split the analog range of both axis into this number of slices.
+     * A changed callback will be fire each time the increment changes. 
+     * You can also set increments on individial axis after this has 
+     * been called.
+     * @param numIncr 
+     */
+    void setNumIncrements(uint8_t numIncr=10);
+
+    /**
+     * @brief Normally increments are set with setNumIncrements but
+     * you can also set the negative and positive sides individually.
+     * 
+     * @param numIncr 
+     */
+    void setNumNegativeIncrements(uint8_t numIncr=10);
+    /**
+     * @brief Normally increments are set with setNumIncrements but
+     * you can also set the negative and positive sides individually.
+     * 
+     * @param numIncr 
+     */
+    void setNumPositiveIncrements(uint8_t numIncr=10);
+
+    void setCentreBoundary(uint16_t width=200);
+    void setOuterBoundary(uint16_t width=100);
+
+    /**
+     * Set the idle timeout in ms (default 10000)
+     * Note: this sets the idle timeout for both EventAnalog axis.
+     * You can override them individually after setting this.
+     */
+    void setIdleTimeout(unsigned int timeoutMs);
+
+    /**
+     * Set an identifier, ignored by library (not unique, defaults to 0)
+     * Useful when calling a common handler.
+     * Note: Does not set the EventAnalog userId
+     */
+    void setUserId(unsigned int id);
+
+    /**
+     * Set the user state. Slight function creep but useful.
+     * For use by implementor, ignored by library
+     * eg ON, OFF, INACTIVE etc
+     * Note: Does not set the EventAnalog userState
+     */
+    void setUserState(unsigned int s);
+
+
+    /**
+     * @brief Returns true of the position of either EventAnalog
+     * axis has changed since previous update()
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool hasChanged();
+
+    /**
+     * Get the identifier (not unique, defaults to 0)
+     */
+    unsigned int userId();
+
+    /**
+     * Get the user state.
+     */
+    unsigned int userState();
+
+
+    /**
+     * Returns true if enabled or if either of the EventAnalg axis are enabled
+     */
+    bool enabled();
+
+    /**
+     * Set enabled to true (default) or false
+     * This will enable/disable all event callbacks.
+     * If allowRead is set to true, will still do analogRead
+     * to allow manual setting of max negative/positive values without
+     * firing callbacks
+     * This is applied to both EventAnalog axis.
+     */
+    void enable(bool e=true, bool allowRead=false);
+
+    /**
+     * @brief Return true if no activity on both EventAnalog
+     * axis for  longer than setIdleTimeout - irrespective of 
+     * whether the idle (or changed) callback has been fired
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool isIdle();
+    
+
+
+  protected:
+    
+    //Encoder
+    EventJoystickCallback changed_cb = NULL;
+    EventJoystickCallback idle_cb = NULL;
+
+  private:
+    unsigned int _userId = 0;
+    unsigned int _userState = 0;
+
+
+
+};
+
+#endif
+
+
