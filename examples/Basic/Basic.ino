abb8643fea0cb533ead9b808fff0aad036adc798
Philip Fletcher <philip.fletcher@stutchbury.com>
2022-02-08 15:57:07 +0000

    Add examples

---
+++ examples/Basic/Basic.ino
@@ -0,0 +1,46 @@
+/**
+ * A basic example of using the EventJoystick library.
+ */
+#include <EventJoystick.h>
+
+/**
+ * Instantiate an EventJoystick.
+ * Must be analog pins
+ */
+EventJoystick ej1(A0,A1);
+
+
+/**
+ * A function to handle the 'changed' event for either
+ * the x or y axis.
+ * Can be called anything but requires EventJoystick& 
+ * as its only parameter.
+ */
+void onEj1Changed(EventJoystick& ej) {
+  Serial.print("ej1 changed. X position: ");
+  Serial.print(ej.x.position());
+  Serial.print(", Y position: ");
+  Serial.println(ej.y.position());
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("EventJoystick Basic Example");
+
+  ej1.setNumIncrements(50);
+  //Link the event(s) you require to your function
+  ej1.setChangedHandler(onEj1Changed);
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /**
+   * You must call update() for every defined EventJoystick.
+   * This will read the state of the both axis in the
+   * EventJoystick and fire the appropriate events.
+   */
+  ej1.update();
+}
---
+++ examples/IndividualAxisCallbacks/IndividualAxisCallbacks.ino
@@ -0,0 +1,60 @@
+/**
+ * An example of using the EventJoystick library with callbacks for induvidual axis.
+ */
+#include <EventJoystick.h>
+
+/**
+ * Instantiate an EventJoystick.
+ * Must be analog pins
+ */
+EventJoystick ej1(A0,A1);
+
+/**
+ * A function to handle the X axis 'changed' event
+ * Can be called anything but requires EventAnalog& 
+ * as its only parameter.
+ * Note: Here we are specifying an EventAnalog type
+ * not an EventJoystick.
+ */
+void onEj1XChanged(EventAnalog& ej) {
+  Serial.print("ej1 changed. X position: ");
+  Serial.println(ej.position());
+}
+
+/**
+ * A function to handle the Y axis 'changed' event
+ * Can be called anything but requires EventAnalog& 
+ * as its only parameter.
+ * Note: Here we are specifying an EventAnalog type
+ * not an EventJoystick.
+ */
+void onEj1YChanged(EventAnalog& ej) {
+  Serial.print("ej1 changed. Y position: ");
+  Serial.println(ej.position());
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("EventJoystick Individual Axis Callback Example");
+
+  //Link the event(s) you require to your function
+  /**
+   * Here we are setting the changed handlers on the individual axis.
+   */
+  ej1.x.setChangedHandler(onEj1XChanged);
+  ej1.y.setChangedHandler(onEj1YChanged);
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /**
+   * You must call update() for every defined EventJoystick.
+   * This will read the state of the both axis in the
+   * EventJoystick and fire the appropriate events.
+   * Do not call the individual axis update() method.
+   */
+  ej1.update();
+}
